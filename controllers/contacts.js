16bea90739beb0becd6c5caf649523358e50f236
VasilenkoYurii <yura.vasilenko98@gmail.com>
2023-05-17 12:20:07 +0300

    add ctrl and made the short code

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,51 +1,72 @@
-const Contact = require("../models/contact");
+const { Contact } = require("../models/contact");
+const { ctrlWrapper } = require("../helpers");
 
-// const fs = require("fs/promises");
-// const path = require("path");
-// const { nanoid } = require("nanoid");
-
-// const contactsPath = path.join(__dirname, "contacts.json");
-
-const listContacts = async () => {
+const listContacts = async (req, res) => {
   const data = await Contact.find();
-  return data;
+  res.json(data);
 };
 
-const getContactById = async (contactId) => {
-  // const result = await Contact.findOne({ _id: contactId });
-  const result = await Contact.findById(contactId);
-  return result || null;
+const getContactById = async (req, res) => {
+  const result = await Contact.findById(req.params.contactId);
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
+  res.json(result);
 };
 
-const addContact = async (body) => {
-  const result = await Contact.create(body);
+const addContact = async (req, res) => {
+  const result = await Contact.create(req.body);
+  res.status(201).json(result);
+};
+
+const updateContact = async (req, res) => {
+  const result = await Contact.findByIdAndUpdate(
+    req.params.contactId,
+    req.body,
+    {
+      new: true,
+    }
+  );
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
+
+  res.status(200).json(result);
   return result;
 };
 
-const updateContact = async (contactId, body) => {
-  const result = await Contact.findByIdAndUpdate(contactId, body, {
-    new: true,
+const updateStatusContact = async (req, res) => {
+  const result = await Contact.findByIdAndUpdate(
+    req.params.contactId,
+    req.body,
+    {
+      new: true,
+    }
+  );
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
+
+  res.status(200).json(result);
+  return result;
+};
+
+const removeContact = async (req, res) => {
+  const result = await Contact.findByIdAndRemove(req.params.contactId);
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
+
+  res.json({
+    message: "Delete success",
   });
-  return result;
-};
-
-const updateStatusContact = async (contactId, body) => {
-  const result = await Contact.findByIdAndUpdate(contactId, body, {
-    new: true,
-  });
-  return result;
-};
-
-const removeContact = async (contactId) => {
-  const result = await Contact.findByIdAndRemove(contactId);
-  return result;
 };
 
 module.exports = {
-  listContacts,
-  getContactById,
-  updateStatusContact,
-  removeContact,
-  addContact,
-  updateContact,
+  listContacts: ctrlWrapper(listContacts),
+  getContactById: ctrlWrapper(getContactById),
+  updateStatusContact: ctrlWrapper(updateStatusContact),
+  removeContact: ctrlWrapper(removeContact),
+  addContact: ctrlWrapper(addContact),
+  updateContact: ctrlWrapper(updateContact),
 };
--- models/contact.js
+++ models/contact.js
@@ -1,4 +1,6 @@
 const { Schema, model } = require("mongoose");
+const { handleMongooseError } = require("../helpers");
+const Joi = require("joi");
 
 const contactSchema = new Schema({
   name: {
@@ -17,6 +19,27 @@ const contactSchema = new Schema({
   },
 });
 
+contactSchema.post("save", handleMongooseError);
+
+const addSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().required(),
+  phone: Joi.string().required(),
+  favorite: Joi.boolean().required(),
+});
+
+const updateFavoriteSchema = Joi.object({
+  favorite: Joi.boolean().required(),
+});
+
+const schemas = {
+  addSchema,
+  updateFavoriteSchema,
+};
+
 const Contact = model("contact", contactSchema);
 
-module.exports = Contact;
+module.exports = {
+  Contact,
+  schemas,
+};
