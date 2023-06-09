055778c2097fd8711b5314d20540165ee8935bcb
VasilenkoYurii <yura.vasilenko98@gmail.com>
2023-06-09 09:45:15 +0300

    add jest test  for login fun

--- controllers/auth.test.js
+++ controllers/auth.test.js
@@ -1,29 +1,21 @@
+const axios = require("axios");
 const ctrl = require("../controllers/auth");
 
 describe("Login function", () => {
-  test("should return status code 200, token, and user object with email and subscription fields", () => {
-    const req = {
-      body: {
-        email: "liva@gmail.com",
-        password: "112233!Awd",
-      },
-    };
-    const res = {
-      json: jest.fn(),
+  test("the data is peanut butter", async () => {
+    const body = {
+      email: "liva@gmail.com",
+      password: "112233!Awd",
     };
 
-    return ctrl.login(req, res).then(() => {
-      console.log(req);
-      //   expect(res.json).toHaveBeenCalledTimes(1);
-      //   const result = res.json.mock.calls[0][0];
-      //   expect(result).toHaveProperty("token");
-      //   expect(result).toHaveProperty("user");
-      //   expect(typeof result.token).toBe("string");
-      //   expect(typeof result.user).toBe("object");
-      //   expect(result.user).toHaveProperty("email");
-      //   expect(result.user).toHaveProperty("subscription");
-      //   expect(typeof result.user.email).toBe("string");
-      //   expect(typeof result.user.subscription).toBe("string");
-    });
-  }, 30000);
+    const respons = await axios.post(
+      "http://localhost:3000/api/auth/login",
+      body
+    );
+
+    expect(respons.status).toBe(200);
+    expect(typeof respons.data.email).toBe("string");
+    expect(typeof respons.data.subscription).toBe("string");
+    expect(typeof respons.data.token).toBe("string");
+  });
 });
